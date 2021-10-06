d142de6d19dd703568b578b748f0c4ad46405de5
PkonovLOV <Pmkonovalov@gmail.com>
2021-10-06 23:32:41 +0300

    add homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,17 +1,23 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+# """
+# Домашнее задание №1
+# Функции и структуры данных
+# """
 
-
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-
+#
+# def power_numbers():
+#     """
+#     функция, которая принимает N целых чисел,
+#     и возвращает список квадратов этих чисел
+#     >>> power_numbers(1, 2, 5, 7)
+#     <<< [1, 4, 25, 49]
+#     """
+def power_numbers(numbers):
+    result = list()
+    for i in numbers:
+        print(i)
+        square = i ** 2
+        result.append(square)
+    return result
 
 # filter types
 ODD = "odd"
@@ -19,14 +25,43 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+# def filter_numbers():
+#     """
+#     функция, которая на вход принимает список из целых чисел,
+#     и возвращает только чётные/нечётные/простые числа
+#     (выбор производится передачей дополнительного аргумента)
+#
+#     # >>> filter_numbers([1, 2, 3], ODD)
+#     # <<< [1, 3]
+#     # >>> filter_numbers([2, 3, 4, 5], EVEN)
+#     # <<< [2, 4]
+#     # """
+def filter_numbers(list_of_numbers, tip):
+    print(tip)
+    elements = []
+    for num in list_of_numbers:
+        if tip == ODD:
+            if num % 2 != 0:
+                print(num, "Это  не четное число")
+                elements.append(num)
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+        elif tip == EVEN:
+            if num % 2 == 0:
+                print(num, "Это четное число")
+                elements.append(num)
+
+        elif tip == PRIME:
+            if num > 1:
+                for i in range(2, num):
+                    if (num % i) == 0 :
+                        print(num, "Это не простое число")
+                        break
+                else:
+                    print(num, "Это   простое число")
+                    elements.append(num)
+            else:
+                print(num, "Это не простое число")
+        else:
+            print("Type Error")
+
+    return elements
